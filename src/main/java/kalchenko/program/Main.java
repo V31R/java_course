0f9d8e8807ffd97e31ffc53bcf930de93111b30a
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2021-12-02 14:36:19 +0300

    For review

--- src/main/java/kalchenko/program/Main.java
+++ src/main/java/kalchenko/program/Main.java
@@ -1,8 +1,6 @@
 package kalchenko.program;
 
 import kalchenko.input_class.TerminalReader;
-import kalchenko.task.TaskListController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
