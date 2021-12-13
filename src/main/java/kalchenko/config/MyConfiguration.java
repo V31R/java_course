58b94281c96734a5a3ba6179091c5889b3f35d35
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2021-12-14 00:38:36 +0300

    Tried to add validation

---
+++ src/main/java/kalchenko/config/MyConfiguration.java
@@ -0,0 +1,9 @@
+package kalchenko.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@EnableWebMvc
+@Configuration
+public class MyConfiguration {
+}
