9070f8d2de3efc4416d6f15b8d80e66a32a8f15d
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2021-12-27 11:43:36 +0300

    Added jasypt for encrypting properties

--- src/main/java/kalchenko/Main.java
+++ src/main/java/kalchenko/Main.java
@@ -1,16 +1,46 @@
 package kalchenko;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 @SpringBootApplication
 public class Main{
 
+    @Value("${spring.datasource.driver-class-name}")
+    private String dbDriverClassName;
+
+    @Value("${spring.datasource.url}")
+    private String dbUrl;
+
+    @Value("${spring.datasource.username}")
+    private String dbUsername;
+
+    @Value("${spring.datasource.password}")
+    private String dbPassword;
+
+    @Bean
+    public DataSource dataSource() {
+
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(dbDriverClassName);
+        dataSource.setUrl(dbUrl);
+        dataSource.setUsername(dbUsername);
+        dataSource.setPassword(dbPassword);
+        return dataSource;
+
+    }
+
     public static  void  main(String[] args){
 
-        SpringApplication.run(Main.class,args);
+        SpringApplication.run(Main.class, args);
+
 
     }
 
