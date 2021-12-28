a4152912b18430c4f0b369921f2665cb5e6772d6
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2021-12-28 18:16:20 +0300

    For review

--- src/main/java/kalchenko/Main.java
+++ src/main/java/kalchenko/Main.java
@@ -1,18 +1,10 @@
 package kalchenko;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
-
-import javax.sql.DataSource;
-
 
 @SpringBootApplication
 public class Main{
-
 
     public static  void  main(String[] args){
 
@@ -20,6 +12,5 @@ public class Main{
 
 
     }
-
 
 }
