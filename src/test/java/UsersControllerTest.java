387a5a918ce2dd68f51c61d2caeacd835f2679c4
kalchenko.vladislaw <kalchenko.vladislaw@yandex.ru>
2022-02-10 01:37:53 +0300

    Added UsersController

--- src/test/java/UsersControllerTest.java
+++ src/test/java/UsersControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.platform.suite.api.Suite;
 import static org.junit.jupiter.api.Assertions.*;
 import org.mockito.Mockito;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.ArrayList;
@@ -30,6 +31,26 @@ public class UsersControllerTest {
 
     }
 
+    @Test
+    public void testPut(){
+
+        Users users =new Users();
+        Users usersSpy = Mockito.spy(users);
+
+        UserRepository userRepositoryMock = Mockito.mock(UserRepository.class);
+
+        Mockito.when(userRepositoryMock.save(usersSpy)).thenReturn(usersSpy);
+
+        PasswordEncoder passwordEncoderMock= Mockito.mock(PasswordEncoder.class);
+        Mockito.when(passwordEncoderMock.encode("")).thenReturn("hash");
+
+
+        UsersController usersController = new UsersController(userRepositoryMock,passwordEncoderMock);
+
+        assertNotEquals(usersSpy.getPassword(),usersController.createUser(usersSpy).getPassword());
+        assertEquals(usersSpy.getUserID(),usersController.createUser(usersSpy).getUserID());
+
+    }
 
 
 }
